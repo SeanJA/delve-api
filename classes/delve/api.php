2a819a66f51cfdc91bbd69d75629a038e4817e37
SeanJA <sean.sandy@gmail.com>
2012-01-27 08:15:25 -0400

    added caching to the delve api class Added /cache to the gitignore, and nbproject

--- classes/delve/api.php
+++ classes/delve/api.php
@@ -13,11 +13,28 @@ class delve_api {
 	}
 
 	protected function get_request($query, $params = array()) {
-		$url = 'http://api.delvenetworks.com/rest/organizations/' . $this->org_id . $query . '.' . $this->format;
+		if($this->is_cached($query)){
+			$response = $this->get_cached($query);
+		} else {
+			$url = 'http://api.delvenetworks.com/rest/organizations/' . $this->org_id . $query . '.' . $this->format;
 
-		$signed_get_usage_info_url = Delve_Auth::authenticate_request("GET", $url, $this->access_key, $this->secret, $params);
-		$response = file_get_contents($signed_get_usage_info_url);
+			$signed_get_usage_info_url = Delve_Auth::authenticate_request("GET", $url, $this->access_key, $this->secret, $params);
+			$response = file_get_contents($signed_get_usage_info_url);
+			$this->cache_result($query, $response);
+		}
 		return $this->response = $this->decode($response);
+	}
+	
+	private function cache_result($query,$result){
+		file_put_contents(ROOT.'/cache/'.md5($query), $result);
+	}
+	
+	private function is_cached($query){
+		return file_exists(ROOT.'/cache/'.md5($query));
+	}
+	
+	private function get_cached($query){
+		return file_get_contents(ROOT.'/cache/'.md5($query));
 	}
 
 	public function decode($response) {
--- report_media.php
+++ report_media.php
@@ -1,5 +1,4 @@
 <?php
-
 include 'config.php';
 $d = new delve_analytics();
 $d->access_key = ACCESS_KEY;
@@ -8,25 +7,26 @@ $d->org_id = ORG_ID;
 
 $d->report_media();
 ?>
+
 <table>
 	<tr>
 		<th>Title</th>
 		<th>Plays</th>
 		<th>Time Wasted</th>
 	</tr>
-<?php foreach($d->response as $r): ?>
-	<tr>
-		<td>
-			<?php echo $r->media_title; ?>
-		</td>
-		<td>
-			<?php echo $r->times_plays; ?>
-		</td>
-		<td>
-			<span title="<?php echo $r->total_time_viewed ?>">
-				<?php echo time_wasted($r->total_time_viewed); ?>
-			</span>
-		</td>
-	</tr>
-<?php endforeach; ?>
+	<?php foreach ($d->response as $r): ?>
+		<tr>
+			<td class="media" id="<?php echo $r->media_id; ?>">
+				<?php echo $r->media_title; ?>
+			</td>
+			<td>
+				<?php echo $r->times_plays; ?>
+			</td>
+			<td>
+				<span title="<?php echo $r->total_time_viewed ?>">
+					<?php echo time_wasted($r->total_time_viewed); ?>
+				</span>
+			</td>
+		</tr>
+	<?php endforeach; ?>
 </table>
